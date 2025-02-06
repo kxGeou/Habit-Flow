27cf08371684c1a527018d97782e31e4100a3446
kxGeou <grzegorzm.work@gmail.com>
2025-02-06 23:07:50 +0100

    Update

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,11 +1,29 @@
 import Description from "../Components/ui/Description";
 import Tutorial from "./Tutorial";
+import { useState, useRef, useEffect } from "react";
+export default function AboutUs() {
+    const ref = useRef<HTMLElement | null>(null);
+    const [isIntersecting, setIsIntersecting] = useState(false);
+  
+    useEffect(() => {
+      if (!ref.current) return; 
+  
+      const observer = new IntersectionObserver(([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      });
+  
+      observer.observe(ref.current);
+      console.log(isIntersecting);
+      return () => observer.disconnect();
+    }, [ref.current]);
 
-export default function AboutUs(){
-    return(
-        <section className="my-[6rem] flex justify-between items-center">
-            <Description title="Take control of your habits" description="HabitFlow helps you track daily habits, stay consistent, and achieve your goals. Customize your habit blocks, visualize progress, and build a better routine—one day at a time"></Description>
-            <Tutorial></Tutorial>
-        </section>
-    )
+  return (
+      <section className="my-[6rem] flex justify-between items-center " ref={ref}>
+        <Description
+          title="Take control of your habits"
+          description="HabitFlow helps you track daily habits, stay consistent, and achieve your goals. Customize your habit blocks, visualize progress, and build a better routine—one day at a time"
+        ></Description>
+        <Tutorial></Tutorial>
+      </section>
+  );
 }
